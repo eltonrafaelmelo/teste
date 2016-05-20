ce7f1dffe667e32659e42a2f84c1f7d926ba2761
eltonrafaelmelo <elton.rafaelmelo@gmail.com>
2016-05-20 09:57:08 -0300

    view

--- teste storyboard/ViewController.swift
+++ teste storyboard/ViewController.swift
@@ -14,10 +14,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func teste () {
+        print("merge teste")
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func goll () {
+        print("gol")
     }
 
 
